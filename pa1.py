94ba30821a402baf1d32999b8139fed7403f080f
issa402 <is.jimenezinzone@gmail.com>
2025-02-17 22:36:15 -0500

    Continuation on algorithm and maze

--- pa1.py
+++ pa1.py
@@ -18,3 +18,33 @@ class Grid:
                 n.append(nbrsX, nbrsY)
 
     def maze(self): #Back tracking using DFS
+        stack = deque()
+        start = (random.randint(1, self.size -1), random.randint(1, self.size-1))#starting position of the random cell
+        self.grid[start] = 1
+        stack.append(start)
+
+        while stack:
+            current = stack.pop()
+            n = []
+            for neighbor in self.neighbors(current):
+                if self.grid[neighbor] == 0:
+                    n.append(neighbor)
+            
+            if n:
+                stack.append(current)
+                new = random.choice(n)
+                if random.random() == 0.3 :
+                    self.grid[new] = -1 #Blocked Cell
+                else:
+                    self.grid[new] = 1 #Unblock indentifier
+                    stack.append(new)
+
+                if not stack:
+                    unvisited = np.argwhere(self.grid == 0)
+                    if unvisited.size > 0:
+                        start = tuple(unvisited[0])
+                        self.grid[start]= 1
+                        stack.append(start)
+
+            
+
